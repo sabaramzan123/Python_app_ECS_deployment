f1780e0f8ff100e196ce9468b6b68ca1409f9507
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-21 21:09:18 +0500

    Fix: add iam tf file

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,44 @@
+# -----------------------------------------------
+# ECS EXECUTION ROLE
+# -----------------------------------------------
+resource "aws_iam_role" "ecs_execution" {
+  name = "${var.project_name}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = "arn:aws:partition:aws::policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# -----------------------------------------------
+# ECS TASK ROLE (app ko AWS services access dene ke liye)
+# -----------------------------------------------
+resource "aws_iam_role" "ecs_task" {
+  name = "${var.project_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}

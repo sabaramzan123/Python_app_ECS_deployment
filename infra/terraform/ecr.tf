049c09e3930e491625553c5fb55fa2441c9920ef
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-21 20:51:11 +0500

    Fix: fix ecr terraform file

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "app" {
-  name                 = var.project_name
+  name                 = "${var.project_name}-compiler-app"  # ← match karao pipeline se
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {

d507045352a3c80494f52f0095475428d54cb808
sabaramzan123 <sabaramzan0226@gmail.com>
2026-05-21 21:38:51 +0500

    Fix: fix terraform files

--- infra/terraform/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  azs = ["${var.aws_region}a", "${var.aws_region}b"]
-
-  common_tags = {
-    Project     = var.project_name
-    Environment = var.environment
-    ManagedBy   = "terraform"
-  }
-}
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -5,8 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {}
 }
 
 provider "aws" {
